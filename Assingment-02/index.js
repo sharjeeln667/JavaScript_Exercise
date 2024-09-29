0abdb6ebbcf338bc6cbef73c4e28c415b4a88312
sharjeel <sharjeeln667@gmail.com>
2024-09-30 03:26:08 +0500

    Assignment-03

---
+++ Assingment-02/index.js
@@ -0,0 +1,63 @@
+// Declare 3 variables in one statement
+var var1, var2, var3;
+
+// Legal variable names
+var firstName;
+var _age;
+var $salary;
+var user1;
+var camelCaseVariable;
+
+// Illegal variable names
+// var 1name;      // Illegal: Starts with a number
+// var @address;   // Illegal: Contains a special character @
+// var user-name;  // Illegal: Contains a hyphen
+// var var ;        // Illegal: Uses a reserved keyword
+// var first name; // Illegal: Contains a space
+
+// var num1 = 3
+// var num2 = 5
+
+// var sum = num1 + num2
+// console.log(sum);
+
+
+// var num1 = 10
+// var num2 = 3
+
+// var sum = num1 - num2
+// console.log(sum);
+
+// var num1 = 9
+// var num2 = 7
+
+// var sum = num1 * num2
+// console.log(sum);
+
+// var num1 = 100
+// var num2 = 10
+
+// var sum = num1 / num2
+// console.log(sum);
+
+
+
+// var number;
+// number = 5
+// number++
+// number += 7
+// number--
+// var remainder = number % 3
+
+// console.log("The OutPut Is " + remainder);
+
+// var ticket = 600
+// var totalCost = ticket * 5
+// console.log("Total cost of 5 tikcets is " + totalCost);
+
+// var number = 5
+
+// var result = number * 1
+
+// console.log(result);
+
---
+++ Assingment-03/index.js
@@ -0,0 +1,76 @@
+
+// var checkValues = 65;
+
+// if (checkValues >= 48 && checkValues <= 57) {
+//     console.log('The character is a number.');
+// } else if (checkValues >= 65 && checkValues <= 90) {
+//     console.log('The character is an uppercase letter.');
+// } else if (checkValues >= 97 && checkValues <= 122) {
+//     console.log('The character is a lowercase letter.');
+// } else {
+//     console.log('The character is neither a number nor a letter.');
+// }
+
+
+// var num1 = 5;
+// var num2 = 5;
+// if (num1 > num2) {
+//     console.log(num1 + ' is larger.');
+// } else if (num2 > num1) {
+//     console.log(num2 + ' is larger.');
+// } else {
+//     console.log('Both numbers are equal.');
+// }
+
+
+// var number = 0;
+// if (number > 0) {
+//     console.log('The number is positive.');
+// } else if (number < 0) {
+//     console.log('The number is negative.');
+// } else {
+//     console.log('The number is zero.');
+// }
+
+
+
+// var char = "h";
+// var result = (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u');
+// console.log(result ? 'True, it is a vowel.' : 'False, it is not a vowel.');
+
+// var correctPassword = 'mypassword';
+// var userPassword = "dhjehfirewf";
+// if (userPassword === '') {
+//     console.log('Please enter your password.');
+// } else if (userPassword === correctPassword) {
+//     console.log('Correct! The password you entered matches the original password.');
+// } else {
+//     console.log('Incorrect password.');
+// }
+
+
+
+// var time = 5;
+
+// var greeting;
+
+// if (time < 18) {
+//     greeting = 'Good Morning';
+// } else {
+//     greeting = 'Good evening';
+// }
+// console.log(greeting);
+
+
+// var time = 17;
+// if (time >= 0 && time <= 12) {
+//     console.log('Good Morning!');
+// } else if (time > 12 && time <= 16) {
+//     console.log('Good Afternoon!');
+// } else if (time > 16 && time <= 20) {
+//     console.log('Good Evening!');
+// } else if (time > 20 && time <= 24) {
+//     console.log('Good Night!');
+// } else {
+//     console.log('Invalid time input!');
+// }
